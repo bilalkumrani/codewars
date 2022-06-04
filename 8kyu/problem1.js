11ba30652e694ac60078ff07937868c9b3794105
bilal <me@example.com>
2022-06-04 18:59:03 +0500

    problem1

---
+++ 8kyu/problem1.js
@@ -0,0 +1,22 @@
+// description
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+// my solution
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+
+  return result;
+};
